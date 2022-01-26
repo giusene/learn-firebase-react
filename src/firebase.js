ea3540d1b170e11ed41465918dea27bfd67e7819
g.sen <giu.sene@gmail.com>
2022-01-26 13:39:21 +0100

    fix: env keys

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,12 @@ import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_APIKEY,
-  authDomain: process.env.FIREBASE_AUTHDOMAIN,
-  projectId: process.env.FIREBASE_PROJECTID,
-  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.FIREBASE_APPID
+  authDomain: `${process.env.FIREBASE_AUTHDOMAIN}`,
+  projectId: `${process.env.FIREBASE_PROJECTID}`,
+  storageBucket: `${process.env.FIREBASE_STORAGEBUCKET}`,
+  messagingSenderId: `${process.env.FIREBASE_MESSAGINGSENDERID}`,
+  appId: `${process.env.FIREBASE_APPID}`
 };
-
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
